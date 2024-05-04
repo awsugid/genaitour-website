a6aba611c3172547d221d4da2cd3816a51a881de
Rio Astamal <me@rioastamal.net>
2024-05-04 12:47:18 +0000

    Simple build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Build script to copy src/ to public/
+
+# Make sure file .genaitour exists in root directory
+[ ! -f .genaitour ] && {
+    printf "Error: .genaitour not found.\n" >&2
+    exit 1
+}
+
+# Remove old files
+printf "Removing old files...\n"
+rm -rf public/*
+
+# Copy new files
+printf "Copying new files...\n"
+cp -r src/* public/
